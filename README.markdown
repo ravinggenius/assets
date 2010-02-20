716123b740b7b8248d114b80a38180e6e3160d05
Raving Genius <rg+code@ravinggenius.com>
2010-02-20 09:47:52 -0500

    Added sizzle

---
+++ README.markdown
@@ -0,0 +1,1 @@
+Experimental Sinatra project using Compass.
--- app.rb
+++ app.rb
@@ -1,11 +1,28 @@
 require 'rubygems'
+require 'compass'
 require 'sinatra'
 require 'haml'
 
 require 'models/concatenation'
 require 'models/asset'
 
-set :root, File.dirname(__FILE__)
+configure do
+  set :app_file, __FILE__
+  set :root, File.dirname(__FILE__)
+  set :haml, { :format => :html5, :attr_wrapper => '"' }
+
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views/stylesheets'
+  end
+
+  set :sass, Compass.sass_engine_options
+end
+
+get '/styles/:name.css' do
+  content_type :css, :charset => 'utf-8'
+  sass :"stylesheets/#{params[:name]}"
+end
 
 get '/' do
   haml :index
@@ -21,8 +38,4 @@ end
 get '/v' do
   @assets = Asset.all
   haml :versions
-end
-
-not_found do
-  haml :'errors/not_found'
 end
